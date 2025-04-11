155cb9736a11e5d54ade25f79ae98fdd19f0bba6
1.9.8.2.6 (AJM) <74386189+ajm19826@users.noreply.github.com>
2025-04-11 19:09:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,5 @@
+function getGame()
+{
 document.write("<title>AstaCode</title><h1>Games</h1><iframe src='https://visualbasic7.github.io/dif'></iframe><style>iframe{width:100%; height:100%;}</style>");
+}
+//Game || MONKEYGG2 FORK
